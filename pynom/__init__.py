0ba1cba1f580757da13260ba407717493813968a
Charles Machalow <csm10495@gmail.com>
2022-03-27 12:37:00 -0700

    Add a digestion feature to have exceptions disappear if caught after a certain amount of time
    Also run code through black/isort

--- pynom/__init__.py
+++ pynom/__init__.py
@@ -1,3 +1,3 @@
-from pynom.pynom import PyNom, CombinedException, ExceptionInfo
+from pynom.pynom import CombinedException, ExceptionInfo, PyNom
 
-__version__ = '0.0.9'
+__version__ = "0.0.10"
--- pynom/test_pynom.py
+++ pynom/test_pynom.py
@@ -1,10 +1,17 @@
+import datetime
+from typing import List
+from unittest.mock import MagicMock
+
 import pytest
-from . import PyNom, CombinedException, ExceptionInfo
+
+from . import CombinedException, ExceptionInfo, PyNom
+
 
 def test_pynom_allows_a_single_exception_type():
     pynom = PyNom(ZeroDivisionError, 3)
     assert pynom.exception_types_to_eat[0] == ZeroDivisionError
     assert len(pynom.exception_types_to_eat) == 1
+
 
 def test_simple_pynom():
     pynom = PyNom([ZeroDivisionError], 3)
@@ -17,14 +24,15 @@ def test_simple_pynom():
 
     try:
         with pynom:
-            1/ 0
+            1 / 0
     except Exception as ex:
         assert isinstance(ex, ZeroDivisionError)
         assert isinstance(ex, CombinedException)
     else:
-        assert 0, 'pynom did not raise'
+        assert 0, "pynom did not raise"
 
     assert len(pynom._exception_information[ZeroDivisionError]) == 0
+
 
 def test_pynom_custom_throw_up_action():
     def throw_up_action(ex):
@@ -40,9 +48,10 @@ def test_pynom_custom_throw_up_action():
 
     with pytest.raises(ValueError):
         with pynom:
-            raise ValueError('i am the breaking point')
+            raise ValueError("i am the breaking point")
 
-    assert throw_up_action.val, 'throw_up_action was not called?'
+    assert throw_up_action.val, "throw_up_action was not called?"
+
 
 def test_pynom_can_eat_all_exceptions():
     pynom = PyNom([PyNom.ALL_EXCEPTIONS], 1)
@@ -63,6 +72,7 @@ def test_pynom_can_eat_all_exceptions():
         with pynom:
             raise ValueError
 
+
 def test_pynom_wont_eat_not_listed_exception():
     pynom = PyNom([ValueError], 1)
 
@@ -70,12 +80,14 @@ def test_pynom_wont_eat_not_listed_exception():
         with pynom:
             raise EnvironmentError
 
+
 def test_no_exception_should_not_throw_up():
-    pynom = PyNom([PyNom.ALL_EXCEPTIONS], 1, throw_up_action=lambda x: 1/0)
+    pynom = PyNom([PyNom.ALL_EXCEPTIONS], 1, throw_up_action=lambda x: 1 / 0)
 
     for i in range(3):
         with pynom:
             pass
+
 
 def test_pynom_will_call_side_dish_action():
     def side_dish_action(ex):
@@ -105,12 +117,93 @@ def test_pynom_will_call_side_dish_action():
         raise EOFError
     assert side_dish_action.call_count == 4
 
+
 def test_pynom_will_not_call_side_dish_action_if_there_is_no_exception():
     def side_dish_action(ex):
-        raise Exception("side_dish_action should not have been called since there was no exception")
+        raise Exception(
+            "side_dish_action should not have been called since there was no exception"
+        )
 
     pynom = PyNom(PyNom.ALL_EXCEPTIONS, 2, side_dish_action=side_dish_action)
 
     for i in range(10):
         with pynom:
             pass
+
+
+def test_pynom_digestion_works():
+    digested: List[ExceptionInfo] = []
+
+    def digest_action(ex_info):
+        digested.append(ex_info)
+
+    pynom = PyNom(
+        PyNom.ALL_EXCEPTIONS,
+        3,
+        digest_action=digest_action,
+        digest_time=datetime.timedelta(microseconds=0.00000000000001),
+    )
+
+    # a fail here could mean not enough time passed (in which case add a tiny sleep)
+    for i in range(5):
+        with pynom:
+            raise ValueError()
+
+    assert len(digested) == 5
+    assert [type(ex_info.exception) for ex_info in digested] == [ValueError] * 5
+
+
+def test_pynom_digestion_too_slow():
+    digested: List[ExceptionInfo] = []
+
+    def digest_action(ex_info):
+        digested.append(ex_info)
+
+    pynom = PyNom(
+        PyNom.ALL_EXCEPTIONS,
+        3,
+        digest_action=digest_action,
+        digest_time=datetime.timedelta(seconds=1),
+    )
+
+    with pytest.raises(ValueError):
+        for i in range(5):
+            with pynom:
+                raise ValueError()
+
+
+def test_pynom_check_and_perform_digestion_digest_time_none():
+    pynom = PyNom(PyNom.ALL_EXCEPTIONS, 3, digest_action=MagicMock(), digest_time=None)
+    pynom._check_and_perform_digestion(ValueError, None)
+
+
+def test_pynom_check_and_perform_digestion_digest_time_works():
+    digested: List[ExceptionInfo] = []
+
+    def digest_action(ex_info):
+        digested.append(ex_info)
+
+    pynom = PyNom(
+        PyNom.ALL_EXCEPTIONS,
+        3,
+        digest_action=digest_action,
+        digest_time=datetime.timedelta(days=1),
+    )
+
+    for i in range(2):
+        with pynom:
+            raise ValueError()
+
+    pynom._check_and_perform_digestion(
+        ValueError, datetime.datetime.now() + datetime.timedelta(days=3)
+    )
+
+    assert len(digested) == 2
+    assert [type(ex_info.exception) for ex_info in digested] == [ValueError] * 2
+
+    for i in range(2):
+        with pynom:
+            raise ValueError()
+
+    assert len(digested) == 2
+    assert [type(ex_info.exception) for ex_info in digested] == [ValueError] * 2
